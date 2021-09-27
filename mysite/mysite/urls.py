a56d849a5bee4469d02387c209630ba97f260a3c
Your Name <you@example.com>
2021-09-28 00:31:52 +0500

    Added view and template for detailed blog post as well as CSS for the site

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.post_list, name = 'post_list')
+    path('',views.post_list, name = 'post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
